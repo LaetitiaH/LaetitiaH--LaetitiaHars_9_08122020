321c1dc4d79bba84d19e8137d3fcdf6ba4062557
Laetitia Hars <laetitia.hars@gmail.com>
2021-07-26 17:34:37 +0200

    🎨 improve code format

--- src/views/ErrorPage.js
+++ src/views/ErrorPage.js
@@ -1,8 +1,7 @@
-import VerticalLayout from './VerticalLayout.js'
+import VerticalLayout from "./VerticalLayout.js";
 
 export default (error) => {
-
-  return (`
+  return `
     <div class='layout'>
       ${VerticalLayout()}
       <div class='content'>
@@ -12,6 +11,5 @@ export default (error) => {
         <div data-testid="error-message">
           ${error ? error : ""}
         </div>
-    </div>`
-  )
-}
+    </div>`;
+};
