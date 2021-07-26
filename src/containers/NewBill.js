b1f7bea52f2d12200adfb06d0f336b9af57d45e2
Laetitia Hars <laetitia.hars@gmail.com>
2021-07-26 23:46:18 +0200

    :bug: fix Bills bug when add invalid format file

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -16,47 +16,67 @@ export default class NewBill {
     this.fileName = null;
     new Logout({ document, localStorage, onNavigate });
   }
+
   handleChangeFile = (e) => {
+    $(".invalid-format").remove();
     const file = this.document.querySelector(`input[data-testid="file"]`)
       .files[0];
-    const filePath = e.target.value.split(/\\/g);
-    const fileName = filePath[filePath.length - 1];
-    this.firestore.storage
-      .ref(`justificatifs/${fileName}`)
-      .put(file)
-      .then((snapshot) => snapshot.ref.getDownloadURL())
-      .then((url) => {
-        this.fileUrl = url;
-        this.fileName = fileName;
-      });
+
+    if (file.type === "image/jpeg" || file.type === "image/png") {
+      const filePath = e.target.value.split(/\\/g);
+      const fileName = filePath[filePath.length - 1];
+      this.firestore.storage
+        .ref(`justificatifs/${fileName}`)
+        .put(file)
+        .then((snapshot) => snapshot.ref.getDownloadURL())
+        .then((url) => {
+          this.fileUrl = url;
+          this.fileName = fileName;
+        });
+    } else {
+      this.invalidFileFormat();
+    }
   };
   handleSubmit = (e) => {
+    $(".invalid-format").remove();
     e.preventDefault();
-    console.log(
-      'e.target.querySelector(`input[data-testid="datepicker"]`).value',
-      e.target.querySelector(`input[data-testid="datepicker"]`).value
-    );
-    const email = JSON.parse(localStorage.getItem("user")).email;
-    const bill = {
-      email,
-      type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
-      amount: parseInt(
-        e.target.querySelector(`input[data-testid="amount"]`).value
-      ),
-      date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
-      vat: e.target.querySelector(`input[data-testid="vat"]`).value,
-      pct:
-        parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) ||
-        20,
-      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`)
-        .value,
-      fileUrl: this.fileUrl,
-      fileName: this.fileName,
-      status: "pending",
-    };
-    this.createBill(bill);
-    this.onNavigate(ROUTES_PATH["Bills"]);
+    if (!this.fileUrl) {
+      this.invalidFileFormat();
+    } else {
+      console.log(
+        'e.target.querySelector(`input[data-testid="datepicker"]`).value',
+        e.target.querySelector(`input[data-testid="datepicker"]`).value
+      );
+      const email = JSON.parse(localStorage.getItem("user")).email;
+      const bill = {
+        email,
+        type: e.target.querySelector(`select[data-testid="expense-type"]`)
+          .value,
+        name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
+        amount: parseInt(
+          e.target.querySelector(`input[data-testid="amount"]`).value
+        ),
+        date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
+        vat: e.target.querySelector(`input[data-testid="vat"]`).value,
+        pct:
+          parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) ||
+          20,
+        commentary: e.target.querySelector(`textarea[data-testid="commentary"]`)
+          .value,
+        fileUrl: this.fileUrl,
+        fileName: this.fileName,
+        status: "pending",
+      };
+      this.createBill(bill);
+      this.onNavigate(ROUTES_PATH["Bills"]);
+    }
+  };
+
+  invalidFileFormat = () => {
+    const fileInput = this.document.querySelector(`input[data-testid="file"]`);
+    $(
+      "<p class='invalid-format'>Le format du fichier est invalide</p>"
+    ).insertAfter(fileInput);
   };
 
   // not need to cover this function by tests
